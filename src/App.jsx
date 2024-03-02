9c1f3ae8cfd581e98060a17c6af061cad4e24857
rloza4 <rjlozano58@gmail.com>
2024-03-02 12:35:23 -0600

    day 2

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  function handleCount(){
+    setCount(count+1);
+  }
+
+  return (
+    <>
+      <h1>Count: {count}</h1>
+      <button onClick={handleCount} >Set Count</button>
+    </>
+  )
+}
+
+export default App
